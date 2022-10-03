b5db954c58f244d95cac6e1c8bd06e8307d7a1b4
ooANAoo <rkawkaos77@naver.com>
2022-10-03 20:50:00 +0900

    주석추가
    mainactivity, useradapter주석담

--- app/src/main/java/com/example/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/MainActivity.kt
@@ -13,40 +13,48 @@ import com.google.firebase.database.*
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var  userRecyclerView: RecyclerView
-    private lateinit var userList: ArrayList<User>
-    private  lateinit var adapter: UserAdapter
-    private lateinit var  mAuth: FirebaseAuth
-    private  lateinit var mDbRef: DatabaseReference
+    private lateinit var userList: ArrayList<User>//각 유저들의 데이터를 가지고있는 배열
+    private  lateinit var adapter: UserAdapter// 데이터와 뷰를 이어주는 중간다리
+    private lateinit var  mAuth: FirebaseAuth // 파이어베이스 유저관련 접속하기위한 변수
+    private  lateinit var mDbRef: DatabaseReference// 파이어베이스 리얼타임베이스 접근하기위한 변수
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)//
         setContentView(R.layout.activity_main)
 
-        mAuth = FirebaseAuth.getInstance()
+
+        //유저,리얼타임데이터베이스에 접근 해서 값얻을수있는 통로뚥기 저 두변수가 길이다.
+        mAuth = FirebaseAuth.getInstance()// 나자신의 유저정보 m이 my의 약자다
         mDbRef = FirebaseDatabase.getInstance().getReference()
-
+        //userList 생성자 호출
         userList = ArrayList()
+        //어뎁터 생성자. 여기서 이걸해줌으로 각 어뎁터는 하나의 포지션을같는다 연결해버리니까
         adapter = UserAdapter(this, userList)
-
+        //리사이클러뷰 찾아서 잡아주기
         userRecyclerView = findViewById(R.id.userRecyclerView)
-
+        //유저 리사이클러뷰를 어떤식으로 보이게할지 LinearLayout형식으로
         userRecyclerView.layoutManager = LinearLayoutManager(this)
+        //어뎁터를 넣어준다. 이거때문에 포지션값이 알아서 정의되는거일지도
         userRecyclerView.adapter = adapter
 
-        mDbRef.child("user").addValueEventListener(object: ValueEventListener{
+
+
+        mDbRef.child("user").addValueEventListener(object: ValueEventListener{// 파이어베이스에서 데이터 읽고 쓸수있는 리스너
             override fun onDataChange(snapshot: DataSnapshot) {
 
-                userList.clear()
-                for(postSnapshot in snapshot.children){
+                userList.clear()//리스트 초기화
+                for(postSnapshot in snapshot.children){//user데이터베이스에 있는 모든 리스트들이 처음부터 끝까지 끝날떄 까지
 
-                    val currentUser = postSnapshot.getValue(User::class.java)
+                    val currentUser = postSnapshot.getValue(User::class.java)//user 데이터 베이스에있는 값을 첫번째 유저 부터 가져옴
+                    //위의 for문을 볼 때 반복문임.
 
-                    if(mAuth.currentUser?.uid != currentUser?.uid){
-                        userList.add(currentUser!!)
+                    if(mAuth.currentUser?.uid != currentUser?.uid){//내가 현재 참조된 유저가 아니면
+                        userList.add(currentUser!!)//유저리스트에 내자신은 없다. 유저리스트에 참조된 유저 넣기
                     }
                 }
-                adapter.notifyDataSetChanged()
+                adapter.notifyDataSetChanged()//어뎁터에게 새로운 유저정보가 리스트에 들어와서 리스트의 크기와 정보가 바뀔거라는 알림.
+                // 이녀석이 포지션 잡아줌
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -61,9 +69,19 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if(item.itemId == R.id.logout){
+
+            mAuth.signOut()
+            val intent = Intent(this@MainActivity, Login::class.java)
+            finish()
+            startActivity(intent)
+            return true
+        }
+        //여기서 처리
+        if(item.itemId == R.id.setProfile){
 
             mAuth.signOut()
             val intent = Intent(this@MainActivity, Login::class.java)
--- app/src/main/java/com/example/chatapplication/UserAdapter.kt
+++ app/src/main/java/com/example/chatapplication/UserAdapter.kt
@@ -10,38 +10,40 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 
 class UserAdapter(val context: Context, val userList: ArrayList<User>):
-    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {//Adapter한개 정의
 
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {//화면에 띄울 뷰 한개 생성
         val view: View = LayoutInflater.from(context).inflate(R.layout.user_layout, parent, false)
         return UserViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {// 뷰 생성시 홀더와 포지션값은 정해짐
+        // 방금 생성한 뷰가 화면에서 몇번쨰인지포지션을통해 알아내고 user의정보가 들어있는
+        // userlist도 순서는동일하기에 유저리스트[position]을 통해 값을 커런트 유저로 넣어줌
+        val currentUser = userList[position]//커런트유저는 데이터임
 
-        val currentUser = userList[position]
+        holder.textName.text = currentUser.name//커런트유터 데이터에서 화면인 홀더로 이름을 넘겨줘서 띄울수있게함
 
-        holder.textName.text = currentUser.name
+        holder.itemView.setOnClickListener {//목록의 뷰를 누를시
+            val intent = Intent(context,ChattActivity::class.java)// 화면전환할 액티비티 정의
 
-        holder.itemView.setOnClickListener {
-            val intent = Intent(context,ChattActivity::class.java)
-
+            //전환할 엑티비티로 데이터 넘기기 받는쪽에선 getExtra씀
             intent.putExtra("name", currentUser.name)
             intent.putExtra("uid",currentUser.uid)
 
 
-            context.startActivity(intent)
+            context.startActivity(intent)//환면전환하기.
         }
 
     }
 
-    override fun getItemCount(): Int {
+    override fun getItemCount(): Int {//유저리스트가 몇개인가 회원가입 한사람 수 
         return userList.size
     }
 
-    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){// 누를수있는 뷰로 만들기
         val textName = itemView.findViewById<TextView>(R.id.txt_name)
     }
 
